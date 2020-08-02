b9b435a5ca12b1f7d0b91454f787305c11110b6d
yuta kimura <rubystudy0123@gmail.com>
2020-08-02 23:39:53 +0900

    Implemented search function with ransack

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: '斉藤', age: 35)
+User.create(name: '田中', age: 21)
+User.create(name: '佐藤', age: 32)
+User.create(name: '山岸', age: 33)
+User.create(name: '平', age: 26)
+User.create(name: '加藤', age: 25)
